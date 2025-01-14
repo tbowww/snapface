87bd3ee50589a3ed3115186b93d2cbca809ea1a9
Thibault Etienne <Thibault.Etienne@arhs-digital.com>
2025-01-14 11:13:31 +0100

    Initial commit with first app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FaceSnapComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  mySnap2!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.mySnap2 = new FaceSnap(
+      'Flowers',
+      'Smells nice',
+      'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg',
+      new Date(),
+      0
+    );
+  }
 }
---
+++ src/app/models/face-snap.ts
@@ -0,0 +1,22 @@
+export class FaceSnap {
+    title: string;
+    description: string;
+    createdDate: Date;
+    snaps: number;
+    imageUrl: string;
+    
+    constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number) {
+      this.title = title;
+      this.description = description;
+      this.imageUrl = imageUrl;
+      this.createdDate = createdDate;
+      this.snaps = snaps;
+    }
+
+    snap() {
+      this.snaps++;
+    }
+    unSnap() {
+      this.snaps--;
+    }
+  }
